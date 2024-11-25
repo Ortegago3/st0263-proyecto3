1b5e283f80b8e15e0d36c014f17656a0e74e292d
Ortegago3 <Hortegag@eafit.edu.co>
2024-11-24 19:58:27 -0500

    Datos simulando sql

--- scripts/ingestion_mysql.py
+++ scripts/ingestion_mysql.py
@@ -1,28 +1,27 @@
-import pymysql
 import pandas as pd
 from io import StringIO
 import boto3
 
 BUCKET_NAME = "covid-data-pipeline"
 AWS_REGION = "us-east-1"
-DB_HOST = "mysql-instance.cq2lxqz6odba.us-east-1.rds.amazonaws.com"
-DB_USER = "admin"
-DB_PASSWORD = "password123"
-DB_NAME = "covid_database"
 
 def ingestion_mysql():
-    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
-    query = "SELECT * FROM patient_data"
-    
-    df = pd.read_sql(query, conn)
+    # Simulación de datos
+    data = {
+        "patient_id": [1, 2, 3],
+        "name": ["John Doe", "Jane Smith", "Alice Brown"],
+        "age": [30, 25, 35],
+        "diagnosis": ["COVID-19", "Recovered", "Vaccinated"]
+    }
+    df = pd.DataFrame(data)
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
     
+    # Cargar datos simulados a S3
     s3 = boto3.client('s3', region_name=AWS_REGION)
     s3.put_object(Bucket=BUCKET_NAME, Key="raw/mysql_patient_data.csv", Body=csv_buffer.getvalue())
-    print(f"Datos extraídos y cargados a S3: s3://{BUCKET_NAME}/raw/mysql_patient_data.csv")
-    conn.close()
+    print(f"Datos simulados cargados a S3: s3://{BUCKET_NAME}/raw/mysql_patient_data.csv")
 
 if __name__ == "__main__":
     ingestion_mysql()
